50053a68745e2bf6353086d2ea9db9752b6285aa
fayoz <abdurashidovfayoz7@gmail.com>
2024-01-22 17:42:42 +0500

    code

--- count_jobs.py
+++ count_jobs.py
@@ -1,11 +1,18 @@
 def count_jobs(data:list, job:str) -> int:
-    """
-    Return the number of users with a given job
-
-    Args:
-        data (dict): A dictionary of values
-        job (str): The job to search for
-    Returns:
-        int: The number of users with the given job
-    """
-    return 
+    k=0
+    for i in data:
+        if i['job']==job:
+          k+=1
+    return k
+data=[
+  {
+    'name': 'John', 
+    'job': 'Developer'
+  }, 
+  {
+    'name': 'Mary', 
+    'job': 'Developer'
+  }
+  ]
+job = 'Developer'
+print(count_jobs(data,job))
